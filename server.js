e4c68f472cb9830b3df46de97d70ef68fcfb29fc
ranggi santoso <ranggisantoso@ranggis-MacBook-Pro.local>
2022-12-06 15:21:26 +0700

    revise url

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express  = require("express");
+// const express  = require("express");
+// const app = express()
+// const port = process.env.PORT || 3000;
+// app.use(express.json())
+
+// app.get('health-check', (request, response) => {
+//     response.json({
+//         message:'Health check sucess'
+//     })
+// })
+
+// app.listen(port, () =>{
+//   console.log(`server running at port ${port}`);  
+// })
+
+
+const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = 3000
 app.use(express.json())
-
-app.get('health-check', (request, response) => {
-    response.json({
-        message:'Health check sucess'
-    })
-})
-
-app.listen(port, () =>{
-  console.log(`server running at port ${port}`);  
-})
+app.get('/', (request, response) =>{
+    response.json({message:'Health check success'})})
+    app.listen(port, () =>{
+        console.log(`server running at port ${port}`)})
